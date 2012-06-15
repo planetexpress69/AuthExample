bf86ea496754691bd8277bd74ca54486ba1dfe2a
Martin Kautz <martin@teambender.de>
2012-06-15 21:03:58 +0200

    Initial Commit

---
+++ AuthExample/TBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TBAppDelegate.h
+//  AuthExample
+//
+//  Created by Martin Kautz on 15.06.12.
+//  Copyright (c) 2012 JAKOTA Cruise Systems GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AuthExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AuthExample
+//
+//  Created by Martin Kautz on 15.06.12.
+//  Copyright (c) 2012 JAKOTA Cruise Systems GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TBAppDelegate class]));
+    }
+}
